c4155a81ebd10b214b457dd021f99709ed7e2f24
mfdickerson <md.gorilla.boy@gmail.com>
2015-05-18 01:09:33 -0400

    Looks Prettier Now

--- WallBall.js
+++ WallBall.js
@@ -7,15 +7,27 @@ var timeElapsed = 0.0;
 window.onload = function() {
 	
 	var gl = initialize();
+    var g_last = Date.now();
+
 	var room = new World(gl, 9.81, 10);
-    room.addBall()
+    room.addBall(gl, 0.05, 1.0, 0.8);
+    room.addBall(gl, 0.05, 1.0, 0.8);
+    room.addBall(gl, 0.05, 1.0, 0.8);
+    room.addBall(gl, 0.05, 1.0, 0.8);
+    room.addBall(gl, 0.05, 1.0, 0.8);
+
+    //var camera = new Camera(gl, 0, room.size()/2, room.size()/2);
 
 	var viewMatrix = lookAt(vec3(0.0,0.0,2.4), vec3(0.0,0.0,-1.0), vec3(0,1,0));
     gl.uniformMatrix4fv(gl.u_ViewMatrix, false, flatten(viewMatrix));   // set the model transform (setting to identity initially)
 
-	//var camera = new Camera(gl, 0, room.size()/2, room.size()/2);
+    Promise.all([ initializeTexture(gl, 1, 'images/hardwood.jpg')])
+      .then(function () {tick();})
+      .catch(function (error) {alert('Failed to load texture '+  error.message);});
 
-    var g_last = Date.now();
+
+
+
     var tick = function(){
         // update system
         var now = Date.now();
@@ -30,7 +42,7 @@ window.onload = function() {
         requestAnimationFrame(tick);
     };
     
-    tick();
+    //tick();
 }
 
 
@@ -86,6 +98,11 @@ function initialize() {
     gl.uniform1f(u_Coloring, 3.0);
     // END LIGHTING SETUP
 
+    //get location of attribute text_coord
+    gl.a_TexCoord = gl.getAttribLocation(gl.program, "a_TexCoord");
+    gl.enableVertexAttribArray(gl.a_TexCoord);
+    gl.u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+
 
     return gl;
 }
@@ -93,128 +110,18 @@ function initialize() {
 
 function World(gl, gravity, friction) {
 	this.balls = [];
+    this.walls = new Walls(gl);
     this.gravity = 0.6;
     
 
-    var vertexBuffer;
-    var normalBuffer;
-    var indexBuffer;
-    var FSIZE;
-    var numVertices;
 
-    this.addBall = function() {
-        this.balls.push(new Ball(gl, 0.05, 1.0, 0.8)) //Radius, Mass, Bouncinesst
-        this.balls.push(new Ball(gl, 0.05, 1.0, 0.8)) //Radius, Mass, Bouncinesst
-        this.balls.push(new Ball(gl, 0.05, 1.0, 0.8)) //Radius, Mass, Bouncinesst
+    this.addBall = function(gl, radius, mass, bounciness) {
+        this.balls.push(new Ball(gl, radius, mass, bounciness)) //Radius, Mass, Bouncinesst
 
 
     }
-    
-   
-    // vertices of the cube, we are duplicating points because the faces have different normals
-    var vertices  = new Float32Array([
-          1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0,-1.0, 1.0,  1.0,-1.0, 1.0, // front face
-          1.0, 1.0, 1.0,  1.0,-1.0, 1.0,  1.0,-1.0,-1.0,  1.0, 1.0,-1.0, // right face
-          1.0, 1.0,-1.0,  1.0,-1.0,-1.0, -1.0,-1.0,-1.0, -1.0, 1.0,-1.0, // back face
-         -1.0, 1.0,-1.0, -1.0,-1.0,-1.0, -1.0,-1.0, 1.0, -1.0, 1.0, 1.0, // left face
-          1.0, 1.0, 1.0,  1.0, 1.0,-1.0, -1.0, 1.0,-1.0, -1.0, 1.0, 1.0, // top face
-          1.0,-1.0, 1.0, -1.0,-1.0, 1.0, -1.0,-1.0,-1.0,  1.0,-1.0,-1.0, // bottom face
-    ]);
-    
-    
-    var normals = new Float32Array([
-        0.0, 0.0,-1.0,  0.0, 0.0,-1.0,  0.0, 0.0,-1.0,  0.0, 0.0,-1.0, // front face
-       -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, // right face/
-        0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0,1.0,   0.0, 0.0, 1.0, // back face
-        1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0, // left face
-        0.0,-1.0, 0.0,  0.0,-1.0, 0.0,  0.0,-1.0, 0.0,  0.0,-1.0, 0.0, // top face
-        0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0, // bottom face
-    ]);
-    
-
-    
-    var indices = new Uint8Array([
-       //0,1,2,  0,2,3, // front face
-       4,5,6,  4,6,7,   // right face
-       8,9,10, 8,10,11, // back face
-       12,13,14,  12,14,15, // left face
-       16,17,18, 16,18,19, // top face
-       20,21,22, 20,22,23 // bottom face
-    ]);
-    
-    
-    
-    
-    numVertices = indices.length;
-    FSIZE = vertices.BYTES_PER_ELEMENT;
-    
-    
-    vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-        console.log('Failed to create the vertex buffer');
-        return -1;
-    }
-    
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-    
-    
-    normalBuffer = gl.createBuffer();
-    if (!normalBuffer) {
-        console.log('Failed to create the normal buffer');
-        return -1;
-    }
-    
-    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-
-
-    indexBuffer = gl.createBuffer();
-    if (!indexBuffer) {
-        console.log('Failed to create the index buffer');
-        return -1;
-    }
-
-    
-    
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-    
-    
-    
     this.draw = function() {
-        gl.uniformMatrix4fv(gl.u_ModelMatrix, false, flatten(gl.currentTransform));
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-        
-
-        var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-        if (a_Position < 0) {
-            console.log('Failed to get storage location');
-            return -1;
-        }
-        
-        
-        gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0,0);
-        gl.enableVertexAttribArray(a_Position);
-
-        
-        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-        var a_Normal= gl.getAttribLocation(gl.program, 'a_Normal');
-        if (a_Normal < 0) {
-            console.log('Failed to get storage location');
-            return -1;
-        }
-        
-        
-        gl.vertexAttribPointer(a_Normal, 3, gl.FLOAT, false,  0,0);
-        gl.enableVertexAttribArray(a_Normal);
-        
-        
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-        
-        gl.drawElements(gl.TRIANGLES, numVertices, gl.UNSIGNED_BYTE, 0);
-
+        this.walls.draw();
 
         for (i = 0; i < this.balls.length; i++) {
             var ball = this.balls[i];
@@ -225,7 +132,90 @@ function World(gl, gravity, friction) {
         for (i = 0; i < this.balls.length; i++) {
             var ball = this.balls[i];
             ball.draw(); 
-        }   
+        } 
+    }
+
+    function Walls(gl) {
+
+        var vertexBuffer;
+        var normalBuffer;
+        var indexBuffer;
+        var texBuffer;
+        //var FSIZE;
+        //var numVertices;
+        
+       
+        // vertices of the cube, we are duplicating points because the faces have different normals
+        var vertices  = new Float32Array([
+              1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0,-1.0, 1.0,  1.0,-1.0, 1.0, // front face
+              1.0, 1.0, 1.0,  1.0,-1.0, 1.0,  1.0,-1.0,-1.0,  1.0, 1.0,-1.0, // right face
+              1.0, 1.0,-1.0,  1.0,-1.0,-1.0, -1.0,-1.0,-1.0, -1.0, 1.0,-1.0, // back face
+             -1.0, 1.0,-1.0, -1.0,-1.0,-1.0, -1.0,-1.0, 1.0, -1.0, 1.0, 1.0, // left face
+              1.0, 1.0, 1.0,  1.0, 1.0,-1.0, -1.0, 1.0,-1.0, -1.0, 1.0, 1.0, // top face
+              1.0,-1.0, 1.0, -1.0,-1.0, 1.0, -1.0,-1.0,-1.0,  1.0,-1.0,-1.0, // bottom face
+        ]);
+        
+        
+        var normals = new Float32Array([
+            0.0, 0.0,-1.0,  0.0, 0.0,-1.0,  0.0, 0.0,-1.0,  0.0, 0.0,-1.0, // front face
+           -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, // right face/
+            0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0,1.0,   0.0, 0.0, 1.0, // back face
+            1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0, // left face
+            0.0,-1.0, 0.0,  0.0,-1.0, 0.0,  0.0,-1.0, 0.0,  0.0,-1.0, 0.0, // top face
+            0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0, // bottom face
+        ]);
+
+        var textureCoordinates = new Float32Array([
+           1.0, 1.0,  0.0, 1.0, 0.0, 0.0, 1.0, 0.0, // front face
+           1.0, 1.0,  0.0, 1.0, 0.0, 0.0, 1.0, 0.0, // right face
+           1.0, 1.0,  0.0, 1.0, 0.0, 0.0, 1.0, 0.0, // back face
+           1.0, 1.0,  0.0, 1.0, 0.0, 0.0, 1.0, 0.0, // left face
+           1.0, 1.0,  0.0, 1.0, 0.0, 0.0, 1.0, 0.0, // top face
+           1.0, 1.0,  0.0, 1.0, 0.0, 0.0, 1.0, 0.0 // bottom face
+        ]);
+        
+
+        
+        var indices = new Uint8Array([
+           //0,1,2,  0,2,3, // front face
+           4,5,6,  4,6,7,   // right face
+           8,9,10, 8,10,11, // back face
+           12,13,14,  12,14,15, // left face
+           16,17,18, 16,18,19, // top face
+           20,21,22, 20,22,23 // bottom face
+        ]);
+        
+        
+        
+        
+        vertexBuffer = createBuffer(gl, gl.ARRAY_BUFFER, vertices, 'vertex', gl.STATIC_DRAW);
+        normalBuffer = createBuffer(gl, gl.ARRAY_BUFFER, normals, 'normal', gl.STATIC_DRAW);
+        indexBuffer = createBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, indices, 'index', gl.STATIC_DRAW);
+        texBuffer = createBuffer(gl, gl.ARRAY_BUFFER, textureCoordinates, 'texture coordinate', gl.STATIC_DRAW);
+
+        
+        
+        
+        this.draw = function() {
+            gl.activeTexture(gl.TEXTURE0);
+            gl.bindTexture(gl.TEXTURE_2D, gl.createTexture());
+            gl.vertexAttribPointer(gl.a_TexCoord, 2, gl.FLOAT, false,  0,0);
+
+            gl.uniform1i(gl.u_Sampler,1);
+
+            var u_ColorType = gl.getUniformLocation(gl.program, 'u_ColorType');
+            gl.uniform1i(u_ColorType, 1);
+
+            gl.uniformMatrix4fv(gl.u_ModelMatrix, false, flatten(gl.currentTransform));
+
+            enableAttribute(gl, vertexBuffer, 'a_Position', 3, 0, 0);
+            enableAttribute(gl, normalBuffer, 'a_Normal', 3, 0, 0);
+            enableAttribute(gl, texBuffer, 'a_TexCoord', 2, 0, 0);
+            
+            
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+            gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
+        }
     }
 
     /*
@@ -237,19 +227,49 @@ function World(gl, gravity, friction) {
      * the data since it is all the same.
      */
     function Ball(gl, radius, mass, bounciness) {
+
         this.radius = radius;
         this.bounciness = bounciness;
-        this.position = vec3(-1+Math.random()*2,-1+Math.random()*2,-1+Math.random()*2)
-        this.velocity = vec3(-1+Math.random()*2,-1+Math.random()*2,-1+Math.random()*2)
+        this.position = vec3(-1+Math.random()*2,-1+Math.random()*2,-1+Math.random()*2);
+        this.velocity = vec3(-5+Math.random()*10,-5+Math.random()*10,-5+Math.random()*10);
 
+        var vertexBuffer;
+        var indexBuffer;
+        var normalBuffer;
+        var texBuffer;
+
+        /*
+        This throws an error if we don't pass in informatin for the texture Buffer even though we aren't using it. How do we get rid of this?
+
+        */
+
+     /* Initialize the shape */
+        
+        var vertices = [];
+        var normals = [];
+        var textureCoordinates = new Float32Array([])
+        var indices = [];
+        var numSteps = 100;
+
+        // call the initialization function to jumpstart this object
+        setSteps(numSteps);
+        
+        vertexBuffer = createBuffer(gl, gl.ARRAY_BUFFER, vertices, 'vertex', gl.STATIC_DRAW);
+        normalBuffer = createBuffer(gl, gl.ARRAY_BUFFER, normals, 'normal', gl.STATIC_DRAW);
+        indexBuffer = createBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, indices, 'index', gl.STATIC_DRAW);
+        texBuffer = createBuffer(gl, gl.ARRAY_BUFFER, textureCoordinates, 'texture coordinate', gl.STATIC_DRAW);
 
 
 
         this.updatePosition = function () {
             //console.log(timeElapsed);
-            this.velocity[0] = this.velocity[0];
+    
             this.velocity[1] = -gravity*timeElapsed+this.velocity[1];
-            this.velocity[2] = this.velocity[2];
+
+            if (this.position[1] <= -1.0 + this.radius) {
+                this.velocity[0] = this.velocity[0]*0.5;
+                this.velocity[2] = this.velocity[2]*0.5;
+            }
 
             var x = this.position[0] + this.velocity[0]*timeElapsed;
             var y = this.position[1] + this.velocity[1]*timeElapsed;
@@ -278,45 +298,15 @@ function World(gl, gravity, friction) {
         }
 
 
-        /* Initialize the shape */
-        
-        var vertices = [];
-        var indices = [];
-        var numSteps = 100;
-        
-        
-        var vertexBuffer = gl.createBuffer();
-        if (!vertexBuffer) {
-            console.log('Failed to create the buffer object');
-            return -1;
-        }
-        
-       
-        var indexBuffer = gl.createBuffer();
-        if (!indexBuffer) {
-            console.log('Failed to create the buffer object');
-            return -1;
-        }
-        
-        var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-        if (a_Position < 0) {
-            console.log('Failed to get storage location');
-            return -1;
-        }
-        
-       
-        var a_Normal= gl.getAttribLocation(gl.program, 'a_Normal');
-        if (a_Normal < 0) {
-            console.log('Failed to get storage location');
-            return -1;
-        }
+   
+
         
         /*
          * This function initializes the object. It does all of the grunt work computing all of the
          * points and indices. This has been made a function to make this object mutable. We can
          * decide we want to change the number of points and recalculate the shape.
          */ 
-        this.setSteps = function(steps){
+        function setSteps(steps){
             numSteps = steps; // the number of samples per circle
             var step = 2*Math.PI/numSteps; // angular different beteen samples
          
@@ -353,6 +343,9 @@ function World(gl, gravity, friction) {
             
             //convert to the flat form
             vertices = new Float32Array(vertices);
+            normals = new Float32Array(vertices);
+            textureCoordinates = new Float32Array(vertices);
+
             
 
             
@@ -393,16 +386,9 @@ function World(gl, gravity, friction) {
             
             // convert to our flat form
             indices = new Uint16Array(indices);
-           
-           // Load the vertices into the VBO
-           
-            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-        
 
-            // load up the index buffer
-            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+           
+          
 
         }
         
@@ -411,6 +397,20 @@ function World(gl, gravity, friction) {
          * This method is called when we want to draw the shape.
          */
         this.draw = function() {
+            // HAV TO HAVE SOMETHING
+            gl.activeTexture(gl.TEXTURE0);
+            gl.bindTexture(gl.TEXTURE_2D, gl.createTexture());
+            gl.vertexAttribPointer(gl.a_TexCoord, 2, gl.FLOAT, false,  0,0);
+
+            gl.uniform1i(gl.u_Sampler,1);
+
+
+            //
+
+
+
+            var u_ColorType = gl.getUniformLocation(gl.program, 'u_ColorType');
+            gl.uniform1i(u_ColorType, 0)
 
 
             gl.matrixStack.push(gl.currentTransform);   
@@ -419,26 +419,21 @@ function World(gl, gravity, friction) {
             gl.uniformMatrix4fv(gl.u_ModelMatrix, false, flatten(gl.currentTransform));
 
             // need to rebind the array buffer to the appropriate VBO in cse some other buffer has been made active
+        
             
-
-            
-            
-            // Set the association for the position attribute
-            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-            gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0,0);
-            gl.enableVertexAttribArray(a_Position);
-
-            // set the association for the normal attribute
-            gl.vertexAttribPointer(a_Normal, 3, gl.FLOAT, false, 0,0);
-            gl.enableVertexAttribArray(a_Normal);
+            enableAttribute(gl, vertexBuffer, 'a_Position', 3, 0, 0);
+            enableAttribute(gl, normalBuffer, 'a_Normal', 3, 0, 0);
+            enableAttribute(gl, texBuffer, 'a_TexCoord', 2, 0, 0);
             
         
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
         
             // draw the shape
 
             var offset = 0; // keep track of how far into the index list we are
             // draw the north pole traignel fan
+            //console.log(indices);
             gl.drawElements(gl.TRIANGLE_FAN, numSteps+2, gl.UNSIGNED_SHORT,0);
             offset = (numSteps+2)*indices.BYTES_PER_ELEMENT;
             
@@ -455,12 +450,78 @@ function World(gl, gravity, friction) {
             gl.currentTransform = gl.matrixStack.pop();
                 
         }
-        
-        
-        // call the initialization function to jumpstart this object
-        this.setSteps(numSteps);
+    
         
     }
+}
+
+function initializeTexture(gl, textureid, filename) {
+    
+    return new Promise(function(resolve, reject){
+       var texture = gl.createTexture();
+
+        var image = new Image();
+    
+    
+        image.onload = function(){
+            
+            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+            gl.activeTexture(gl.TEXTURE0 + textureid);
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+            
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR); //second time?
+
+            gl.generateMipmap(gl.TEXTURE_2D);
+            resolve();
+        }
+        
+        
+        image.onerror = function(error){
+            reject(Error(filename));
+        }
+    
+        image.src = filename; 
+    });
+}
+
+
+
+/*
+ * This is helper function to create buffers.
+ */
+function createBuffer(gl, destination, data, name, type){
+    var buffer = gl.createBuffer();
+    if (!buffer) {
+        console.log('Failed to create the ',name,' buffer');
+        return -1;
+    }
+    
+    gl.bindBuffer(destination, buffer);
+    gl.bufferData(destination, data, type);
+    return buffer;
+}
+
+
+/*
+ * This is a new helper function to simplify enabling attributes.
+ * Note that this no longer fails if the attribute can't be found. It gives us a warning, but doesn't crash.
+ * This will allow us to use different shaders with different attributes.
+ */ 
+function enableAttribute(gl, buffer, name, size, stride, offset){
+   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+   var attribute = gl.getAttribLocation(gl.program, name);
+   if (attribute >= 0) {
+       gl.vertexAttribPointer(attribute, size, gl.FLOAT, false, 0,0);
+       gl.enableVertexAttribArray(attribute);
+   }else{
+       console.log('Warning: Failed to get ',name );
+
+   }
+
+
 }
 
 
